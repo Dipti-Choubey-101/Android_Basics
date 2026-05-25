074310519e2f8eedd17e015900f5fd9856bd0e63
Dipti Choubey <diptichoubey101@gmail.com>
2026-05-25 11:10:59 +0530

    Update MainActivity.kt

--- button_click_listener_lambda/MainActivity.kt
+++ button_click_listener_lambda/MainActivity.kt
@@ -7,7 +7,6 @@ import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
-
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
