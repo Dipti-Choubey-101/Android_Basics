c8f866d0d0f7bd21fe8bfc6f9b53e8a6b08f5f18
Dipti Choubey <diptichoubey101@gmail.com>
2026-05-27 15:32:02 +0530

    Update MainActivity.kt

--- edittext_input_display/MainActivity.kt
+++ edittext_input_display/MainActivity.kt
@@ -8,8 +8,7 @@ import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
-import androidx.core.view
-    .WindowInsetsCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var myButton: Button
