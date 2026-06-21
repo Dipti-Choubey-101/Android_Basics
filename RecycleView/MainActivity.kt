a314ede1b3d6b94e4dd2053dace0bc28c3e27734
Dipti Choubey <diptichoubey101@gmail.com>
2026-06-21 15:08:11 +0530

    adding recycleview

---
+++ RecycleView/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.recycleview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recycleview.Adaptor.ExampleAdaptor
+import com.example.recycleview.Model.ExampleItem
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adaptor:ExampleAdaptor
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val exampleList=generateList(100)
+
+        adaptor= ExampleAdaptor(this,exampleList)
+        recyclerView=findViewById(R.id.recyclerView)
+        recyclerView.adapter=adaptor
+        recyclerView.layoutManager=LinearLayoutManager(this)
+        recyclerView.setHasFixedSize(true)
+    }
+    private fun generateList(size:Int):MutableList<ExampleItem>{
+        val list= mutableListOf<ExampleItem>()
+
+        for(i in 0 until size){
+            list.add(ExampleItem("Title $i","Description $i"))
+        }
+        return list
+    }
+}
