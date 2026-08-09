49d989a9568c2be95b53038d730b782e322a4ad5
Dipti Choubey <diptichoubey101@gmail.com>
2026-08-09 09:01:11 +0530

    Update Buttons.kt

--- Buttons.kt
+++ Buttons.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-
 class MainActivity : AppCompatActivity() {
     private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
